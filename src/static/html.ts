9f675a7489ea233bfe8978efc08510520d7dc0e6
kino6052 <kino6052@colorado.edu>
2018-10-26 22:27:06 -0600

    Initial Commit

---
+++ src/static/html.ts
@@ -0,0 +1,15 @@
+const SCRIPT = './static/index.js';
+
+export default `
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tree of Abstraction</title>
+  </head>
+  <body>
+    <div id='root'></div>
+    <script src='${SCRIPT}'/></script>
+  </body>
+</html>
+`
